9fadd24545591f2ebce09afc319f5456c1409a82
Md Amaan <49265889+mdamaan853@users.noreply.github.com>
2021-07-10 19:02:52 +0530

    mongo url

--- api/module/mainDB.js
+++ api/module/mainDB.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://165.232.179.213:27017/treevesto?authSource=admin',{ useNewUrlParser: true,useUnifiedTopology: true });
 // mongoose.connect('mongodb://treevesto:Hey@Treevesto#43@165.232.179.213:27017/treevesto?authSource=admin&tls=false"',{ useNewUr   lParser: true,useUnifiedTopology: true });
 mongoose.connect('mongodb+srv://treevesto:Password+123@cluster0.xwsbx.mongodb.net/treevesto?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true });
-// mongoose.connect('mongodb://localhost:27017/treevesto', { useNewUrlParser: true,useUnifiedTopology: true });
   
 module.exports=mongoose
